f751c96cfbf799c1320671cc26948cdbd5f51ede
Thian Warrington <144112431+ThianWarringtonHZ@users.noreply.github.com>
2024-07-04 15:46:05 +0200

    Upgraded api version to 1.0

--- app/ai_module.py
+++ app/ai_module.py
@@ -1,15 +1,20 @@
-import openai
+from openai import OpenAI
 import pathlib
 
 path = str(pathlib.Path(__file__).parent.resolve()) + '\\secret_key.txt'
 print(path)
 file = open(path, "r")
-openai.api_key = file.read()
+
+client = OpenAI(
+  organization='org-HQQB3V2KlBXlCQ1EGvYk6V9D',
+  project='proj_bYcU0YvaWZ6J6LICnfOMIGZM',
+  api_key=file.read()
+)
 
 def get_advice(query):
-    response = openai.Completion.create(
-        engine="gpt-3.5-turbo-instruct",
-        prompt=query,
-        max_tokens=200
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[{"role": "user", "content": query}],
+        stream=False
     )
-    return response.choices[0].text.strip()
+    return response.choices[0].message.content
